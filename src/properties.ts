bf4a84f0ba76306765b629fb92d2d702a761681b
Jeremy Branham <Jeremy@Savantly.net>
2019-08-02 01:41:19 -0500

    initial conversion to Grafana toolkit

---
+++ src/properties.ts
@@ -0,0 +1,8 @@
+const pluginName = 'savantly-heatmap-panel';
+
+const heatmapEditor = `public/plugins/${pluginName}/partials/heatmapEditor.html`,
+  displayEditor = `public/plugins/${pluginName}/partials/displayEditor.html`,
+  lightCss = `public/plugins/${pluginName}/styles/heatmap-light.css`,
+  darkCss = `public/plugins/${pluginName}/styles/heatmap-dark.css`;
+
+export { pluginName, heatmapEditor, displayEditor, lightCss, darkCss };
